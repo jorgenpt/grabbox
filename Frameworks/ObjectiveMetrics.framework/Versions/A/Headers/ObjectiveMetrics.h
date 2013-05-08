805a7b3bf3b0ef7c64a0149b7cd4155438a5447e
Jørgen P. Tjernø <jorgenpt@gmail.com>
2013-05-08 00:06:16 -0700

    Delete ObjectiveMetrics, we're using it from Pod.

--- Frameworks/ObjectiveMetrics.framework/Versions/A/Headers/ObjectiveMetrics.h
+++
@@ -1,9 +0,0 @@
-//
-//  ObjectiveMetrics.h
-//  ObjectiveMetrics
-//
-//  Created by Jørgen P. Tjernø on 3/25/11.
-//  Copyright 2011 devSoft. All rights reserved.
-//
-
-#import <ObjectiveMetrics/DMTracker.h>
