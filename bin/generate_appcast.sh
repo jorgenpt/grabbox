d363803cd36a64dbfdb96b8722c4d87ddc0589b3
Jørgen P. Tjernø <jorgenpt@gmail.com>
2017-06-23 02:25:36 -0700

    Turn on HTTPS again, tag v0.5.1

--- bin/generate_appcast.sh
+++ bin/generate_appcast.sh
@@ -4,5 +4,4 @@ PRIVKEY_PATH="secrets/sparkle_dsa_priv.pem"
 
 cd "$(dirname "$0")/.."
 
-git subtree pull --prefix=pages origin gh-pages
-./sparkle_bin/generate_appcast "$PRIVKEY_PATH" "pages/updates"
+./sparkle_bin/generate_appcast "$PRIVKEY_PATH" "../grabbox_site/updates"
--- bin/push_site.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/.."
-
-git subtree push --prefix=pages origin gh-pages
