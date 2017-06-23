a6c9e4a469079719b60a694eded500242350319b
Jørgen P. Tjernø <jorgenpt@gmail.com>
2017-06-23 00:05:41 -0700

    Switch appcast URL, minor script tweak

--- bin/generate_appcast.sh
+++ bin/generate_appcast.sh
@@ -5,4 +5,4 @@ PRIVKEY_PATH="bin/sparkle_dsa_priv.pem"
 cd "$(dirname "$0")/.."
 
 git subtree pull --prefix=pages origin gh-pages
-./sparkle_bin/generate_appcast "$PRIVKEY_PATH" "docs/updates"
+./sparkle_bin/generate_appcast "$PRIVKEY_PATH" "pages/updates"
